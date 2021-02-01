cbd885278a22b30785c6cde811e94347d74baec6
chloey863 <yangcidai@gmail.com>
2021-02-01 16:12:00 -0600

    create the network layer with Retrofit

--- app/src/main/java/com/chloeproject/newstime/MainActivity.java
+++ app/src/main/java/com/chloeproject/newstime/MainActivity.java
@@ -6,9 +6,17 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.chloeproject.newstime.model.NewsResponse;
+import com.chloeproject.newstime.network.NewsApi;
+import com.chloeproject.newstime.network.RetrofitClient;
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,7 +38,28 @@ public class MainActivity extends AppCompatActivity {
 
         // then, bind action bar and navController
         NavigationUI.setupActionBarWithNavController(this, navController);
+
+
+        // testing a request to News API:
+        NewsApi newsApi = RetrofitClient.newInstance(this).create(NewsApi.class);
+        newsApi.getTopHeadlines("US").enqueue(new Callback<NewsResponse>() {
+            // if response is valid
+            @Override
+            public void onResponse(Call<NewsResponse> call, Response<NewsResponse> response) {
+                if (response.isSuccessful()) {
+                    Log.d("getTopHeadlines", response.body().toString());
+                } else {
+                    Log.d("getTopHeadlines", response.toString());
+                }
+            }
+            // if response has error
+            @Override
+            public void onFailure(Call<NewsResponse> call, Throwable t) {
+                Log.d("getTopHeadlines", t.toString());
+            }
+        });
     }
+
 
     @Override
     public boolean onSupportNavigateUp() {
---
+++ app/src/main/java/com/chloeproject/newstime/network/RetrofitClient.java
@@ -0,0 +1,51 @@
+package com.chloeproject.newstime.network;
+
+import android.content.Context;
+
+import com.chloeproject.newstime.BuildConfig;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+
+//    String API_KEY = BuildConfig.newsAPIKey;
+    private static final String API_KEY = BuildConfig.newsAPIKey; // Fixed: works after "Invalid caches and restart" Android program
+    private static final String BASE_URL = "https://newsapi.org/v2/";
+
+    public static Retrofit newInstance(Context context) {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(new HeaderInterceptor())
+                .build();
+
+        // Retrofit is a type-safe HTTP client for Android and Java.
+        return new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build();
+    }
+
+    /**
+     * Functionality: building Http request header Via the X-Api-Key HTTP header.
+     * Ref docs: https://newsapi.org/docs/authentication
+     */
+    private static class HeaderInterceptor implements Interceptor {
+
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            Request original = chain.request();
+            Request request = original
+                    .newBuilder()
+                    .header("X-Api-Key", API_KEY)
+                    .build();
+            return chain.proceed(request);
+        }
+    }
+}
