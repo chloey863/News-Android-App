7ba217d9c09e42bc6bef33c01a10f2154280ba1a
chloey863 <yangcidai@gmail.com>
2021-02-01 14:06:51 -0600

    home, save, search: three blank fragments

---
+++ app/src/main/java/com/chloeproject/newstime/ui/home/HomeFragment.java
@@ -0,0 +1,31 @@
+package com.chloeproject.newstime.ui.home;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.chloeproject.newstime.R;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    // Note: LayoutInflater will translate the layout (written in xml) to Java language
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
---
+++ app/src/main/java/com/chloeproject/newstime/ui/search/SearchFragment.java
@@ -0,0 +1,30 @@
+package com.chloeproject.newstime.ui.search;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.chloeproject.newstime.R;
+
+public class SearchFragment extends Fragment {
+
+    public SearchFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+}
