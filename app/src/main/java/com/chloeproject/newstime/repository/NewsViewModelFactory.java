3af07271bc3932a0e2b4dab4245533269010b306
chloey863 <yangcidai@gmail.com>
2021-02-02 14:27:03 -0600

    News ViewModel Factory

---
+++ app/src/main/java/com/chloeproject/newstime/repository/NewsViewModelFactory.java
@@ -0,0 +1,31 @@
+package com.chloeproject.newstime.repository;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.chloeproject.newstime.ui.home.HomeViewModel;
+import com.chloeproject.newstime.ui.search.SearchViewModel;
+
+public class NewsViewModelFactory implements ViewModelProvider.Factory {
+    private final NewsRepository repository;
+
+    public NewsViewModelFactory(NewsRepository repository) {
+        this.repository = repository;
+    }
+
+    /**
+     * Implementing the create() method of ViewModelProvider.Factory
+     */
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(HomeViewModel.class)) {
+            return (T) new HomeViewModel(repository);
+        } else if (modelClass.isAssignableFrom(SearchViewModel.class)) {
+            return (T) new SearchViewModel(repository);
+        } else {
+           throw new IllegalStateException("Unknown ViewModel!!");
+        }
+    }
+}
