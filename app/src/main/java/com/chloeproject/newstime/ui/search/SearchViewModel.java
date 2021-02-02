7133a8c1cf359dcc6a8d545f317f9b2ff16a800c
chloey863 <yangcidai@gmail.com>
2021-02-02 14:20:25 -0600

    search view model

--- app/src/main/java/com/chloeproject/newstime/ui/search/SearchViewModel.java
+++ app/src/main/java/com/chloeproject/newstime/ui/search/SearchViewModel.java
@@ -10,4 +10,24 @@ import com.chloeproject.newstime.repository.NewsRepository;
 
 public class SearchViewModel extends ViewModel {
 
+    private final NewsRepository repository;
+    private final MutableLiveData<String> searchInput = new MutableLiveData<>();
+
+    public SearchViewModel(NewsRepository newsRepository) {
+        this.repository = newsRepository;
+    }
+
+    /**
+     * setSearchInput will be called by View
+     */
+    public void setSearchInput(String query) {
+        searchInput.setValue(query);
+    }
+
+    /**
+     * Mapping a response string to the format in repository's searchNews
+     */
+    public LiveData<NewsResponse> searchNews() {
+        return Transformations.switchMap(searchInput, repository::searchNews);
+    }
 }
