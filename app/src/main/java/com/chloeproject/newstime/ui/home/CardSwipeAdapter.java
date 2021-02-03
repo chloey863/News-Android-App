f0897f3f5e317426938f9d7fd096170549502689
chloey863 <yangcidai@gmail.com>
2021-02-02 22:42:50 -0600

    Update CardSwipeAdapter.java

--- app/src/main/java/com/chloeproject/newstime/ui/home/CardSwipeAdapter.java
+++ app/src/main/java/com/chloeproject/newstime/ui/home/CardSwipeAdapter.java
@@ -1,13 +1,82 @@
 package com.chloeproject.newstime.ui.home;
 
-public class CardSwipeAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.chloeproject.newstime.R;
+import com.chloeproject.newstime.databinding.SwipeNewsCardBinding;
+import com.chloeproject.newstime.model.Article;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardSwipeAdapter extends RecyclerView.Adapter<CardSwipeAdapter.CardSwipeViewHolder>{
     // 1. Supporting data:
-    // TODO
+    private List<Article> articles = new ArrayList<>();
+
+    public void setArticles(List<Article> newsList) {
+        articles.clear();
+        articles.addAll(newsList);
+        notifyDataSetChanged();
+    }
 
     // 2. Adapter overrides:
-    // TODO
+    /**
+     * onCreateViewHolder is for providing the generated item views
+     */
+    @NonNull
+    @Override
+    public CardSwipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.swipe_news_card, parent, false);
+        return new CardSwipeViewHolder(view);
+
+    }
+
+    /**
+     * onBindViewHolder is for binding the data with a view
+     */
+    @Override
+    public void onBindViewHolder(@NonNull CardSwipeViewHolder holder, int position) {
+        Article article = articles.get(position);
+        // display news text
+        holder.titleTextView.setText(article.title);
+        holder.descriptionTextView.setText(article.description);
+
+        // display news images using Picasso Library
+        Picasso.get().load(article.urlToImage).into(holder.imageView);
+    }
+
+    /**
+     * getItemCount is for providing the current data collection size for the recycle view
+     */
+    @Override
+    public int getItemCount() {
+        return articles.size();
+    }
 
     // 3. CardSwipeViewHolder:
-    // TODO
+    public static class CardSwipeViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView titleTextView;
+        TextView descriptionTextView;
+
+        public CardSwipeViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            SwipeNewsCardBinding binding = SwipeNewsCardBinding.bind(itemView);
+            imageView = binding.swipeCardImageView;
+            titleTextView = binding.swipeCardTitle;
+            descriptionTextView = binding.swipeCardDescription;
+        }
+    }
+
 
 }
