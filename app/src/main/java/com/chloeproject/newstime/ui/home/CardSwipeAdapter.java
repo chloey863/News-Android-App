af2d8e9c5be74aa183e1e0ac2b897770ba34836d
chloey863 <yangcidai@gmail.com>
2021-02-02 22:37:17 -0600

    swipes news using card stack

---
+++ app/src/main/java/com/chloeproject/newstime/ui/home/CardSwipeAdapter.java
@@ -0,0 +1,13 @@
+package com.chloeproject.newstime.ui.home;
+
+public class CardSwipeAdapter {
+    // 1. Supporting data:
+    // TODO
+
+    // 2. Adapter overrides:
+    // TODO
+
+    // 3. CardSwipeViewHolder:
+    // TODO
+
+}
