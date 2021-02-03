54c3f8bca7d5e59b6d44910d1baaa7a3c304bfa2
chloey863 <yangcidai@gmail.com>
2021-02-02 20:37:33 -0600

    recycle view for news item from search result

---
+++ app/src/main/java/com/chloeproject/newstime/ui/search/SearchNewsAdapter.java
@@ -0,0 +1,13 @@
+package com.chloeproject.newstime.ui.search;
+
+public class SearchNewsAdapter {
+    // 1. Supporting data:
+    // TODO
+
+    // 2. Adapter overrides:
+    // TODO
+
+    // 3. SearchNewsViewHolder:
+    // TODO
+
+}
