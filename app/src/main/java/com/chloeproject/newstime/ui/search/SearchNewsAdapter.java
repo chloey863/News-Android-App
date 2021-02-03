fe79d398c710640e9e88f5913b2235fc708367f1
chloey863 <yangcidai@gmail.com>
2021-02-02 20:46:03 -0600

    SearchNewsAdapter

--- app/src/main/java/com/chloeproject/newstime/ui/search/SearchNewsAdapter.java
+++ app/src/main/java/com/chloeproject/newstime/ui/search/SearchNewsAdapter.java
@@ -1,13 +1,77 @@
 package com.chloeproject.newstime.ui.search;
 
-public class SearchNewsAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.chloeproject.newstime.R; // the Android resource lookup
+import com.chloeproject.newstime.databinding.SearchNewsItemBinding;
+import com.chloeproject.newstime.model.Article;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchNewsAdapter extends RecyclerView.Adapter<SearchNewsAdapter.SearchNewsViewHolder> {
     // 1. Supporting data:
-    // TODO
+    private List<Article> articles = new ArrayList<>();
+
+    public void setArticles(List<Article> newsList) {
+        articles.clear();
+        articles.addAll(newsList);
+
+        // notifyDataSetChanged() is a base adapter class
+        notifyDataSetChanged();
+    }
 
     // 2. Adapter overrides:
-    // TODO
+    /**
+     * onCreateViewHolder is for providing the generated item views
+     */
+    @NonNull
+    @Override
+    public SearchNewsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.search_news_item, parent, false);
+        return new SearchNewsViewHolder(view);
+    }
+
+
+        /**
+         * onBindViewHolder is for binding the data with a view
+         */
+    @Override
+    public void onBindViewHolder(@NonNull SearchNewsViewHolder holder, int position) {
+        Article article = articles.get(position);
+        holder.favoriteImageView.setImageResource(R.drawable.ic_favorite_24dp);
+        holder.itemTitleTextView.setText(article.title);
+    }
+
+    /**
+     * getItemCount is for providing the current data collection size for the recycle view
+     */
+    @Override
+    public int getItemCount() {
+        return articles.size();
+    }
 
     // 3. SearchNewsViewHolder:
-    // TODO
+    public static class SearchNewsViewHolder extends RecyclerView.ViewHolder {
+        ImageView favoriteImageView;
+        ImageView itemImageView;
+        TextView itemTitleTextView;
 
+        public SearchNewsViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            // use promise trick
+            SearchNewsItemBinding binding = SearchNewsItemBinding.bind(itemView);
+            favoriteImageView = binding.searchItemFavorite;
+            itemImageView = binding.searchItemImage;
+            itemTitleTextView = binding.searchItemTitle;
+        }
+    }
 }
