3b3730862c844dffe1e854e2d86b2741d84b75a3
chloey863 <yangcidai@gmail.com>
2021-02-03 18:50:46 -0600

    Update SavedNewsAdapter.java

--- app/src/main/java/com/chloeproject/newstime/ui/save/SavedNewsAdapter.java
+++ app/src/main/java/com/chloeproject/newstime/ui/save/SavedNewsAdapter.java
@@ -1,13 +1,74 @@
 package com.chloeproject.newstime.ui.save;
 
-public class SavedNewsAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.chloeproject.newstime.R;
+import com.chloeproject.newstime.databinding.SavedNewsItemBinding;
+import com.chloeproject.newstime.model.Article;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SavedNewsAdapter extends RecyclerView.Adapter<SavedNewsAdapter.SavedNewsViewHolder> {
     // 1. Supporting data:
-    // TODO
+    private List<Article> articles = new ArrayList<>();
+
+    public void setArticles(List<Article> newsList) {
+        articles.clear();
+        articles.addAll(newsList);
+        notifyDataSetChanged();
+    }
+
 
     // 2. Adapter overrides:
-    // TODO
+    /**
+     * onCreateViewHolder is for providing the generated item views
+     */
+    @NonNull
+    @Override
+    public SavedNewsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.saved_news_item, parent, false);
+        return new SavedNewsViewHolder(view);
+    }
+
+    /**
+     * onBindViewHolder is for binding the data with a view.
+     */
+    @Override
+    public void onBindViewHolder(@NonNull SavedNewsViewHolder holder, int position) {
+        Article article = articles.get(position);
+        holder.authorTextView.setText(article.author);
+        holder.descriptionTextView.setText(article.description);
+    }
+
+    /**
+     * getItemCount is for providing the current data collection size.
+     */
+    @Override
+    public int getItemCount() {
+        return articles.size();
+    }
 
     // 3. SavedNewsViewHolder:
-    // TODO
+    public static class SavedNewsViewHolder extends RecyclerView.ViewHolder {
 
+        TextView authorTextView;
+        TextView descriptionTextView;
+        ImageView favoriteIcon;
+
+        public SavedNewsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            SavedNewsItemBinding binding = SavedNewsItemBinding.bind(itemView);
+            authorTextView = binding.savedItemAuthorContent;
+            descriptionTextView = binding.savedItemDescriptionContent;
+            favoriteIcon = binding.savedItemFavoriteImageView;
+        }
+    }
 }
