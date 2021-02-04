a1622416bb0152e37055db7f67b627154d1080e9
chloey863 <yangcidai@gmail.com>
2021-02-03 18:46:53 -0600

    Create SavedNewsAdapter.java

---
+++ app/src/main/java/com/chloeproject/newstime/ui/save/SavedNewsAdapter.java
@@ -0,0 +1,13 @@
+package com.chloeproject.newstime.ui.save;
+
+public class SavedNewsAdapter {
+    // 1. Supporting data:
+    // TODO
+
+    // 2. Adapter overrides:
+    // TODO
+
+    // 3. SavedNewsViewHolder:
+    // TODO
+
+}
