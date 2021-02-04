c805e9d64b87a463df8a6d10cdc75e1125c9e41f
chloey863 <yangcidai@gmail.com>
2021-02-03 18:43:37 -0600

    SaveFragment updates and xml

--- app/src/main/java/com/chloeproject/newstime/ui/save/SaveFragment.java
+++ app/src/main/java/com/chloeproject/newstime/ui/save/SaveFragment.java
@@ -3,16 +3,25 @@ package com.chloeproject.newstime.ui.save;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.chloeproject.newstime.R;
+import com.chloeproject.newstime.databinding.FragmentSaveBinding;
+import com.chloeproject.newstime.repository.NewsRepository;
+import com.chloeproject.newstime.repository.NewsViewModelFactory;
 
 public class SaveFragment extends Fragment {
+    private FragmentSaveBinding binding;
+    private SaveViewModel viewModel;
 
     public SaveFragment() {
         // Required empty public constructor
@@ -23,12 +32,41 @@ public class SaveFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    /**
+     * Using a reference to the binding. No need to use findViewById for each view. Any views
+     * with an @+id tag will have a binding automatically. It will refer to any views from
+     * the binding directly.
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_save, container, false);
+        binding =  FragmentSaveBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+//        return inflater.inflate(R.layout.fragment_save, container, false);
     }
 
+    /**
+     *
+     * Note: The ViewModel query APIs abstract away the implementation details about networking or
+     * database operations.
+     */
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+        // making query for the saved articles
+        NewsRepository repository = new NewsRepository(getContext());
+        viewModel = new ViewModelProvider(this, new NewsViewModelFactory(repository)).get(SaveViewModel.class);
+        viewModel
+                .getAllSavedArticles()
+                .observe(
+                        getViewLifecycleOwner(),
+                        savedArticles -> {
+                            if (savedArticles != null) {
+                                Log.d("SaveFragment", savedArticles.toString());
+                            }
+                        });
+
+    }
 }
